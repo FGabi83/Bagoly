c86d66d8bce87d9e6fdbda09f7985315a815d1e6
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-02-16 14:22:01 +0100

    dropwdown menu

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,8 +1,11 @@
-function myFunction() {
-    const dropdownNavbar = document.getElementsByTagName("nav");
-    if (dropdownNavbar.style.display === "flex") {
-      dropdownNavbar.style.display = "none";
-    } else {
-      dropdownNavbar.style.display = "flex";
-    }
+// Description: This file contains the javascript code for the dropdown menu.
+const navMenu = document.querySelector(".nav-menu");
+const dropdownNavbar = document.querySelector(".nav");
+
+navMenu.addEventListener("click", () => {
+  if (dropdownNavbar.style.display === "none") {
+    dropdownNavbar.classList.add("show");
+  } else {
+    dropdownNavbar.classList.remove("show");
   }
+});
