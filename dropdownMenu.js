ef423b860a3973db9210d7030df927dac4e90ab9
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-02-16 15:25:22 +0100

    sandvich menu working

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,11 +1,10 @@
 // Description: This file contains the javascript code for the dropdown menu.
-const navMenu = document.querySelector(".nav-menu");
-const dropdownNavbar = document.querySelector(".nav");
-
-navMenu.addEventListener("click", () => {
-  if (dropdownNavbar.style.display === "none") {
-    dropdownNavbar.classList.add("show");
-  } else {
-    dropdownNavbar.classList.remove("show");
-  }
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const navMenu = document.querySelector(".nav-menu");
+    const dropdownNavbar = document.querySelector(".nav");
+  
+    navMenu.addEventListener("click", () => {
+      dropdownNavbar.classList.toggle("show");
+    });
+  });
+  
