d6f4fa2e80fe730ef456ffd3748add5cb2c588f3
FGabi83 <77617324+FGabi83@users.noreply.github.com>
2024-02-21 13:44:33 +0100

    small changes to mobile version dropdown

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,11 +1,11 @@
+// Path: dropdownMenu.js
 // Description: This file contains the javascript code for the dropdown menu.
 document.addEventListener('DOMContentLoaded', function () {
-    const navMenu = document.querySelector(".nav-menu");
-    const dropdownNavbar = document.querySelector(".nav");
+  const navMenu = document.querySelector(".nav-menu");
+  const dropdownNavbar = document.querySelector(".nav");
   
-    navMenu.addEventListener("click", () => {
-      dropdownNavbar.classList.toggle("show");
-      navMenu.classList.toggle("highlight");
-    });
-  });
-  
+  navMenu.addEventListener("click", () => { 
+    dropdownNavbar.classList.toggle("show");
+    navMenu.classList.toggle("highlight");
+  }); 
+}); 
